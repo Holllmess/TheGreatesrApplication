0a36361635a5f43dcba0aac3e1119858beeed980
Holllmess <spasskih221@gmail.com>
2023-02-11 19:36:28 +0300

    Update Program

--- TheGreatesrApplication/Program.cs
+++ TheGreatesrApplication/Program.cs
@@ -1,3 +1,7 @@
+using TheGreatesrApplication.Services.Repository;
+using TheGreatesrApplication.Services;
+using TheGreatesrApplication.DataBasesServices;
+
 namespace TheGreatesrApplication
 {
     public class Program
@@ -13,7 +17,16 @@ namespace TheGreatesrApplication
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddScoped<IAKindRepository, AKindRepository>();
+            builder.Services.AddScoped<ISkillRepository, SkillRepository>();
+            builder.Services.AddScoped<IAKindRepository, AKindRepository>();
+            builder.Services.AddScoped<IASkillRepository, ASkillRepository>();
 
+            DataBaseSQL.host = builder.Configuration["MYSQL_DBHOST"] ?? builder.Configuration.GetConnectionString("MYSQL_DBHOST");
+            DataBaseSQL.port = builder.Configuration["MYSQL_DBPORT"] ?? builder.Configuration.GetConnectionString("MYSQL_DBPORT");
+            DataBaseSQL.password = builder.Configuration["MYSQL_PASSWORD"] ?? builder.Configuration.GetConnectionString("MYSQL_PASSWORD");
+            DataBaseSQL.userid = builder.Configuration["MYSQL_USER"] ?? builder.Configuration.GetConnectionString("MYSQL_USER");
+            DataBaseSQL.database = builder.Configuration["MYSQL_DATABASE"] ?? builder.Configuration.GetConnectionString("MYSQL_DATABASE");
 
             var app = builder.Build();
 
